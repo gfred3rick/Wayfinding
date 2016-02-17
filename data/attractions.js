1b59ad55beae360b5d68442fd25b2543d1767f75
Gus Frederick <gus.a.frederick@gmail.com>
2016-02-17 16:31:17 -0500

    Updated
    Updated

---
+++ data/attractions.js
@@ -0,0 +1,14 @@
+{
+"type": "FeatureCollection",
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 1, "Id": 11, "Descriptio": "Penn State University - Abington Campus", "PlaceID": "A4", "Mun_Name": "Abington Township", "GEOID_10": "4209100156" }, "geometry": { "type": "Point", "coordinates": [ -75.110871759702704, 40.116751397406659 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 2, "Id": 10, "Descriptio": "Abington Art Center", "PlaceID": "A13", "Mun_Name": "Abington Township", "GEOID_10": "4209100156" }, "geometry": { "type": "Point", "coordinates": [ -75.117012455809487, 40.092417248611838 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 3, "Id": 9, "Descriptio": "Curtis Arboretum", "PlaceID": "C11", "Mun_Name": "Cheltenham Township", "GEOID_10": "4209112968" }, "geometry": { "type": "Point", "coordinates": [ -75.146070772361, 40.084027912759574 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 4, "Id": 12, "Descriptio": "Willow Grove Industrial Park", "PlaceID": "UM6", "Mun_Name": "Upper Moreland Township", "GEOID_10": "4209179176" }, "geometry": { "type": "Point", "coordinates": [ -75.132991003245849, 40.161314843858882 ] } }
+
+]
+}
---
+++ data/locations.js
@@ -0,0 +1,30 @@
+{
+"type": "FeatureCollection",
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Id": 2.0, "Label": "Elkins Park (East and West consolidated)", "LAT": 40.072063, "LONG": -75.127241, "X": -8363126.2284599999, "Y": 4876419.8747199997 }, "geometry": { "type": "Point", "coordinates": [ -75.127241140500004, 40.072063439300003 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 1.0, "Label": "Downtown Glenside", "LAT": 40.100537, "LONG": -75.153522, "X": -8366051.8248899998, "Y": 4880562.88111 }, "geometry": { "type": "Point", "coordinates": [ -75.153522220300005, 40.100537447900003 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 4.0, "Label": "Roslyn Valley", "LAT": 40.131642, "LONG": -75.12655, "X": -8363049.2834299998, "Y": 4885090.55016 }, "geometry": { "type": "Point", "coordinates": [ -75.126549931499994, 40.1316415406 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 5.0, "Label": "Keswick Village", "LAT": 40.10596, "LONG": -75.147052, "X": -8365331.56953, "Y": 4881352.0123899998 }, "geometry": { "type": "Point", "coordinates": [ -75.1470520564, 40.105959630299999 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 3.0, "Label": "Jenkintown", "LAT": 40.095516, "LONG": -75.125781, "X": -8362963.7309299996, "Y": 4879832.17286 }, "geometry": { "type": "Point", "coordinates": [ -75.125781400299999, 40.095516309399997 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 6.0, "Label": "Abington", "LAT": 40.114589, "LONG": -75.121799, "X": -8362520.4653899996, "Y": 4882607.9976899996 }, "geometry": { "type": "Point", "coordinates": [ -75.121799478200003, 40.114588711800003 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 7.0, "Label": "Willow Grove", "LAT": 40.141263, "LONG": -75.117024, "X": -8361988.8643, "Y": 4886491.4779899996 }, "geometry": { "type": "Point", "coordinates": [ -75.117024024399996, 40.141262725099999 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 8.0, "Label": "Downtown Hatboro", "LAT": 40.177263, "LONG": -75.106156, "X": -8360778.9884599997, "Y": 4891735.1758099999 }, "geometry": { "type": "Point", "coordinates": [ -75.106155524800002, 40.177262897299997 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 12.0, "Label": "Willow Grove Industrial Park", "LAT": 40.161307, "LONG": -75.132989, "X": -8363766.1027899999, "Y": 4889410.69696 }, "geometry": { "type": "Point", "coordinates": [ -75.132989229299994, 40.161306735899998 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 11.0, "Label": "Penn State University - Abington Campus", "LAT": 40.116743, "LONG": -75.11087, "X": -8361303.80095, "Y": 4882921.62813 }, "geometry": { "type": "Point", "coordinates": [ -75.110869995599998, 40.116743297500001 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 10.0, "Label": "Abington Art Center", "LAT": 40.092409, "LONG": -75.117011, "X": -8361987.38, "Y": 4879380.0266399998 }, "geometry": { "type": "Point", "coordinates": [ -75.117010690699999, 40.0924091535 ] } }
+,
+{ "type": "Feature", "properties": { "Id": 9.0, "Label": "Curtis Arboretum", "LAT": 40.08402, "LONG": -75.146069, "X": -8365222.1361, "Y": 4878159.3331399998 }, "geometry": { "type": "Point", "coordinates": [ -75.146068999099995, 40.084019820400002 ] } }
+
+]
+}
